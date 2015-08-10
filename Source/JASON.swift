7881912d23aec1e08dabc138f8365cf4ae45d2d3
delba <delba@users.noreply.github.com>
2015-08-10 15:36:24 +0200

    Use subscript with external named parameter

--- Source/JASON.swift
+++ Source/JASON.swift
@@ -98,7 +98,7 @@ extension JSON {
         if object == nil { return self }
         
         if let nsArray = nsArray {
-            return JSON(nsArray.secureSubscript(index))
+            return JSON(nsArray[safe: index])
         }
         
         return JSON(object: nil)
@@ -452,10 +452,8 @@ private extension NSArray {
     
         - returns: The element at the given index or nil
     */
-    func secureSubscript(index: Int) -> AnyObject? {
-        if index < 0 || index >= count {
-            return nil
-        }
+    subscript(safe index: Int) -> AnyObject? {
+        guard index >= 0 && index < count else { return nil }
         
         return self[index]
     }
