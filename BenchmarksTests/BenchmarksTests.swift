0aef7730eb856c4e70557e4e8341c01002595c64
delba <delba@users.noreply.github.com>
2016-04-22 08:01:10 +0200

    Extract Struct in Struct.swift

--- BenchmarksTests/BenchmarksTests.swift
+++ BenchmarksTests/BenchmarksTests.swift
@@ -12,57 +12,6 @@ import XCTest
 import JASON
 import SwiftyJSON
 
-struct Struct {
-    let string: String
-    let int: Int
-    let double: Double
-    let float: Float
-    let bool: Bool
-    let array: [AnyObject]
-    let dictionary: [String: AnyObject]
-    let optionalFloat: Float?
-    let optionalString: String?
-    let optionalInt: Int?
-    let optionalDouble: Double?
-    let optionalBool: Bool?
-    let optionalArray: [AnyObject]?
-    let optionalDictionary: [String: AnyObject]?
-    
-    init(jason json: JASON.JSON) {
-        string = json["string"].stringValue
-        int = json["int"].intValue
-        double = json["double"].doubleValue
-        float = json["float"].floatValue
-        bool = json["bool"].boolValue
-        array = json["array"].arrayValue
-        dictionary = json["dictionary"].dictionaryValue
-        optionalString = json["string"].string
-        optionalInt = json["int"].int
-        optionalDouble = json["double"].double
-        optionalFloat = json["float"].float
-        optionalBool = json["bool"].bool
-        optionalArray = json["array"].array
-        optionalDictionary = json["dictionary"].dictionary
-    }
-    
-    init(swityJson json: SwiftyJSON.JSON) {
-        string = json["string"].stringValue
-        int = json["int"].intValue
-        double = json["double"].doubleValue
-        float = json["float"].floatValue
-        bool = json["bool"].boolValue
-        array = json["array"].arrayObject ?? [AnyObject]()
-        dictionary = json["dictionary"].dictionaryObject ?? [String: AnyObject]()
-        optionalString = json["string"].string
-        optionalInt = json["int"].int
-        optionalDouble = json["double"].double
-        optionalFloat = json["float"].float
-        optionalBool = json["bool"].bool
-        optionalArray = json["array"].arrayObject
-        optionalDictionary = json["dictionary"].dictionaryObject
-    }
-}
-
 let object: AnyObject = [
     "string": "string",
     "int": 42,
