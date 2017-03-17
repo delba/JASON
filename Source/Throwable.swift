6360e2965a585637ff339f845706036852522b34
delba <delba@users.noreply.github.com>
2017-03-17 14:12:22 +0100

    Add missingValue and dateFormatting errors

--- Source/Throwable.swift
+++ Source/Throwable.swift
@@ -9,15 +9,21 @@
 import Foundation
 
 enum Error<T: Any>: Swift.Error, CustomStringConvertible {
-    case missing(JASON.Key<T>)
-    case casting(JASON.Key<T>)
+    case missingKey(JASON.Key<T>)
+    case missingValue(JASON.Key<T>)
+    case castingValue(JASON.Key<T>)
+    case dateFormatting(JASON.Key<T>)
 
     var description: String {
         switch self {
-        case let .missing(key):
-            return "Missing key \(key.type)"
-        case let .casting(key):
-            return "Casting error \(key.type)"
+        case let .missingKey(key):
+            return "Missing key '\(key.type)'"
+        case let .missingValue(key):
+            return "Missing value '\(key.type)'"
+        case let .castingValue(key):
+            return "Casting error '\(key.type)'"
+        case let .dateFormatting(key):
+            return "Date formatting error '\(key.type)'"
         }
     }
 }
@@ -25,11 +31,15 @@ enum Error<T: Any>: Swift.Error, CustomStringConvertible {
 extension JSON {
     public func get<T>(_ key: JASON.Key<T>) throws -> T {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let value = object as? T else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         return value
@@ -37,11 +47,15 @@ extension JSON {
 
     public func get<T>(_ key: JASON.Key<[T]>) throws -> [T] {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let value = object as? [T] else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         return value
@@ -49,11 +63,15 @@ extension JSON {
     
     public func get(_ key: JASON.Key<Float>) throws -> Float {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let value = object as? NSNumber else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         return value.floatValue
@@ -61,11 +79,15 @@ extension JSON {
 
     public func get(_ key: JASON.Key<[String: Any]>) throws -> [String: Any] {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let value = object as? [String: Any] else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         return value
@@ -73,15 +95,19 @@ extension JSON {
 
     public func get(_ key: JASON.Key<Date>) throws -> Date {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let string = object as? String else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         guard let date = JSON.dateFormatter.date(from: string) else {
-            throw Error.casting(key)
+            throw Error.dateFormatting(key)
         }
 
         return date
@@ -89,7 +115,11 @@ extension JSON {
 
     public func get(_ key: JASON.Key<JSON>) throws -> JSON {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         return JSON(object)
@@ -97,11 +127,15 @@ extension JSON {
 
     public func get(_ key: JASON.Key<[JSON]>) throws -> [JSON] {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let array = object as? [Any] else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         return array.map { JSON($0) }
@@ -109,11 +143,15 @@ extension JSON {
 
     public func get(_ key: JASON.Key<[String: JSON]>) throws -> [String: JSON] {
         guard let object = self[key.type].object else {
-            throw Error.missing(key)
+            throw Error.missingKey(key)
+        }
+
+        if object is NSNull {
+            throw Error.missingValue(key)
         }
 
         guard let dictionary = object as? [String: Any] else {
-            throw Error.casting(key)
+            throw Error.castingValue(key)
         }
 
         return dictionary.reduceValues { JSON($0) }
